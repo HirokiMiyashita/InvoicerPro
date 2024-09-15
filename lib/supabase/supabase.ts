80ea207f1ca2309b71799752ced4837f9562e35c
HirokiMiyashita <hirokixyzrr@gmail.com>
2024-09-16 08:30:32 +0900

    wip

--- lib/supabase/supabase.ts
+++ lib/supabase/supabase.ts
@@ -1,10 +1,10 @@
-// lib/supabase.ts
-import { createClient } from "@supabase/supabase-js";
+// // lib/supabase.ts
+// import { createClient } from "@supabase/supabase-js";
 
-// SupabaseのURLとキーを環境変数から取得
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-console.log(supabaseUrl);
-console.log(supabaseKey);
-// Supabaseクライアントの作成
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// // SupabaseのURLとキーを環境変数から取得
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+// console.log(supabaseUrl);
+// console.log(supabaseKey);
+// // Supabaseクライアントの作成
+// export const supabase = createClient(supabaseUrl, supabaseKey);
